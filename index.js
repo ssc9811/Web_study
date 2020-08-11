31ceda257a9955f690cf933a1809a945e8c32be0
ssc9811 <poer2418@naver.com>
2020-08-11 15:49:22 +0900

    Modified HTML from JS

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const calculator = {
-    plus: function(a , b){
-        return a + b;
-    }
-}
-
-const plus = calculator.plus(5 , 5);
-console.log(plus);
+const title = document.getElementById("title");
+title.innerHTML = "Hi! from JS"

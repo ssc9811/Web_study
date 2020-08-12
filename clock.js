0e8adbd90a2619db1ce0a12010da7c0c126e25cb
ssc9811 <poer2418@naver.com>
2020-08-12 15:57:52 +0900

    Add Clock, made in js

---
+++ clock.js
@@ -0,0 +1,21 @@
+const clockContainer = document.querySelector(".js-clock"),
+ clockTitle = clockContainer.querySelector("h1");
+ document.getElementsByName
+
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minitues = date.getMinutes();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+        minitues < 10 ? `0${minitues}` : minitues}:${
+        seconds < 10 ? `0${seconds}` : seconds}`;
+
+}
+
+function init() {
+    getTime();
+    setInterval(getTime, 1000); // 1초마다 getTime 함수 초기화  -> setInterval(function , 1000 = 1초)
+}
+
+init();
